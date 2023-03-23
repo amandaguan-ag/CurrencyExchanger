64b6ff57b0c2987705d654d9a7682d5fd0a260fe
amandaguan-ag <amandaguan1314@gmail.com>
2023-03-23 16:49:50 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 // Business Logic
-function getExchange(amount, currency) {
+function getExchange(amount, toCurrency) {
   let request = new XMLHttpRequest();
-  const url = `https://v6.exchangerate-api.com/v6/987dcfb5b314687fca8df694/latest/USD`;
+  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
   request.addEventListener("loadend", function () {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
-      printElements(response, amount, currency);
+      printElements(response, amount, toCurrency);
     }
   });
 
@@ -16,19 +16,21 @@ function getExchange(amount, currency) {
   request.send();
 }
 // UI Logic
-function printElements(apiResponse, amount, currency) {
+function printElements(apiResponse, amount, toCurrency) {
   document.querySelector(
     "#showResponse"
   ).innerText = `${amount} USD is equal to ${
-    apiResponse.conversion_rates[currency] * amount
-  } ${currency}`;
+    apiResponse.conversion_rates[toCurrency] * amount
+  } ${toCurrency}`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
   const amount = document.querySelector("#amount").value;
+  const toCurrency = document.querySelector("#toCurrency").value;
   document.querySelector("#amount").value = null;
-  getExchange(amount, currency);
+  document.querySelector("#toCurrency").value = null;
+  getExchange(amount, toCurrency);
 }
 
 window.addEventListener("load", function () {
