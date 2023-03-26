fdaacc1f3b63c840b40ec39d81f741cddd19c43b
amandaguan-ag <amandaguan1314@gmail.com>
2023-03-25 21:03:06 -0700

    update Promises with API Calls

--- src/index.js
+++ src/index.js
@@ -1,29 +1,57 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+
 // Business Logic
+
 function getExchange(amount, toCurrency) {
-  let request = new XMLHttpRequest();
-  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-  request.addEventListener("loadend", function () {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, amount, toCurrency);
-    } else {
-      printError(this, amount, toCurrency);
-    }
+  let promise = new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+
+    request.addEventListener("loadend", function () {
+      const response = JSON.parse(this.responseText);
+      if (this.status === 200) {
+        resolve(response);
+      } else {
+        reject(this);
+      }
+    });
+
+    request.open("GET", url, true);
+    request.send();
   });
 
-  request.open("GET", url, true);
-  request.send();
+  promise.then(
+    function (response) {
+      printElements(response, amount, toCurrency);
+    },
+    function (request) {
+      printError(request, amount, toCurrency);
+    }
+  );
 }
+
 // UI Logic
-function printElements(apiResponse, amount, toCurrency) {
+function printElements(response, amount, toCurrency) {
+  const amountValue = amount;
+  const toCurrencyValue = toCurrency;
+  const conversionRate = response.conversion_rates[toCurrencyValue];
+  const convertedAmount = amountValue * conversionRate;
+
   document.querySelector(
     "#showResponse"
-  ).innerText = `${amount} USD is equal to ${
-    apiResponse.conversion_rates[toCurrency] * amount
-  } ${toCurrency}`;
+  ).innerText = `${amountValue} USD is equal to ${convertedAmount} ${toCurrencyValue}`;
+}
+
+function printError(request, amount, toCurrency) {
+  const amountValue = amount;
+  const toCurrencyValue = toCurrency;
+  const errorMessage = request.statusText;
+
+  document.querySelector(
+    "#showResponse"
+  ).innerText = `There was an error converting ${amountValue} USD to ${toCurrencyValue}. Error message: ${errorMessage}`;
 }
 
 function handleFormSubmission(event) {
@@ -33,12 +61,6 @@ function handleFormSubmission(event) {
   document.querySelector("#amount").value = null;
   document.querySelector("#toCurrency").value = null;
   getExchange(amount, toCurrency);
-}
-
-function printError(request, amount, toCurrency) {
-  document.querySelector(
-    "#showResponse"
-  ).innerText = `There was an error converting ${amount} USD to ${toCurrency}. Error message: ${request.statusText}`;
 }
 
 window.addEventListener("load", function () {
